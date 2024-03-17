c09dd809fa7a205bd6986036f6f130b1a321fd80
Thirstymob <117647423+AnshKumar6400@users.noreply.github.com>
2024-03-17 10:15:03 +0530

    second commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,26 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Signin from "./pages/Signin";
+import Signup from "./pages/Signup";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
 function App() {
-  return <div className=" bg-slate-800">App</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route>
+          <Route path="/" element={<Home />} />
+          <Route path="/sign-in" element={<Signin/>} />
+          <Route path="/signup" element={<Signup/>}/>
+          <Route path="/profile" element={<Profile/>} />
+          <Route path="/about" element ={<About/>} />
+
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ client/src/pages/Signup.jsx
@@ -0,0 +1,10 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+
+function Signup() {
+  return (
+    <div>Signup</div>
+  )
+}
+
+export default Signup
