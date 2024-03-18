b8bb2d1a27b360d2dfebf8463d72bced8d8c71ff
Thirstymob <117647423+AnshKumar6400@users.noreply.github.com>
2024-03-18 18:04:58 +0530

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,5 +1,13 @@
-const express=require('express')
+const express=require('express');
+const mongoose =require('mongoose')
 const app=express();
+const dotenv=require('dotenv');
+dotenv.config();
+mongoose.connect(process.env.MONGO).then(()=>{
+    console.log('connected');
+}).catch((err)=>{
+    console.log(err);
+})
 app.listen(3000,()=>{
     console.log("Server is running on port 3000")
 })
